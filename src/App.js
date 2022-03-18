48140458fe3a703756978e02bd8d9f1234f0d787
bmalkes <bmalkes@gmail.com>
2022-03-18 14:25:48 +0200

    push first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import List from "./pages/list/List";
+import Single from "./pages/single/Single";
+import New from "./pages/new/New";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/">
+                        <Route index element={<Home />} />
+                        <Route path="/login" component={Login} />
+                        <Route path="users">
+                            <Route index element={<List />} />
+                            <Route path=":userId" element={<Single />} />
+                            <Route path="new" element={<New />} />
+                        </Route>
+                        <Route path="products">
+                            <Route index element={<List />} />
+                            <Route path=":productId" element={<Single />} />
+                            <Route path="new" element={<New />} />
+                        </Route>
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
+};
 
 export default App;
